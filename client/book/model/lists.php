8245b68d2253ce2e30cedf942586d10073d1ca43
Dung <dungnnph37530@fpt.edu.vn>
2023-11-22 23:18:42 +0700

    dung commit7

--- client/book/model/lists.php
+++ client/book/model/lists.php
@@ -1,7 +1,6 @@
 <?php
 
 $allBookType = getRaw("SELECT * FROM book_type");
-
-
+$allBook = getRaw("SELECT b.*, t.name AS 't_name' from book AS b INNER JOIN book_type AS t ON b.book_type_id=t.id $filter");
 
 ?>
