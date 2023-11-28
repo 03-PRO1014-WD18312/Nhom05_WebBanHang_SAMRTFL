4fb04d7f8f6e2b3aab18e2bad95664144937730c
KimHue <phue6203@gmail.com>
2023-11-28 12:20:58 +0700

    hue commit

--- client/home/controller/lists.php
+++ client/home/controller/lists.php
@@ -7,27 +7,19 @@ layout('header', 'client');
 
 <div class="container_my padding_X py-3">
 
-<?php
+    <?php
 
-$msg = getFlashData('msg');
-$type = getFlashData('type');
+    $msg = getFlashData('msg');
+    $type = getFlashData('type');
 
-getAlert($msg, $type);
+    getAlert($msg, $type);
 
-view('menu_banner', 'client');
+    view('menu_banner', 'client');
 
-view('course', 'client');
-
-// view('tearch_hot', 'client');
-
-// view('evaluate', 'client');
-
-view('blog', 'client');
-
-view('register_tc', 'client');
-
-
-?>
+    view('course', 'client');
+    view('blog', 'client');
+    view('register_tc', 'client');
+    ?>
 
 </div>
 
